369e4b78e0a6a2b3e89af7dfe7d0019b80a3ba60
jarockis77 <jarockis77@gmail.com>
2024-01-02 15:23:39 +0200

    lend,add & return added

--- classes.py
+++ classes.py
@@ -1,10 +1,39 @@
 class Library:
-    def __init__(self, booklist, name):
-        self.booklist = booklist
+
+    def __init__(self, bookslist, name):
+        self.bookslist = bookslist
         self.name = name
         self.lenderDict = {}
 
     def displayBooks(self):
         print(f"we have following books in our library:{self.name}")
-        for book in self.booklist:
+        for book in self.bookslist:
             print(book)
+
+    def lendBook(self, book, user):
+
+            if book not in self.lenderDict.keys():
+                self.lenderDict.update({book: user})
+                print("Book has been lended, Database updated.")
+            else:
+                print(f"Book is already being used by {self.lenderDict[book]}")
+        # else:
+        #     print("Apologies! We don't have this book in our library")
+
+    def addBook(self, book):
+        if book in self.bookslist:
+            print("Book already exists")
+        else:
+            self.booksList.append(book)
+            print("Book added")
+
+
+
+    def returnbook(self, book):
+        if book in self.lenderDict.keys():
+            self.lenderDict.pop(book)
+            print("Book Returned Succesfully")
+        else:
+            print("The book does not exist in the book lending database")
+
+
